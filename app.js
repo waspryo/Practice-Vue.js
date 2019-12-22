a3a4c2a78b78f1b9b3b18dda64ebe30d3b1c5a10
waspryo <wasp2123321@gmail.com>
2019-12-22 22:02:35 +0900

    Call a function when the form is submitted

--- app.js
+++ app.js
@@ -2,5 +2,10 @@ const app = new Vue({
     el: '#app',
     data: {
         title: 'Hello Coding Garding'
-    }
+    },
+    methods: {
+        addTodo() {
+            console.log('form submitted')
+        },
+    },
 })
